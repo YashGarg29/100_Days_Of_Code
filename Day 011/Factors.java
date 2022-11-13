e9a7f9a5be5fefbe622862b5f0a307123038537f
YashGarg <yashgarg1106@gmail.com>
2022-11-13 20:57:39 +0530

    Factors

---
+++ Day 011/Factors.java
@@ -0,0 +1,17 @@
+package problems;
+
+public class Factors {
+    public static void main(String[] args) {
+        int num = 60;
+        System.out.println(factor(num));
+    }
+    static int factor (int num) {
+        int count = 0;
+        for (int i = 1; i <= num; i ++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
