09274a82bea10d724e4191abcd3e03ddb1435c72
YashGarg <yashgarg1106@gmail.com>
2022-11-13 12:32:19 +0530

    Sum Of Two Nos

---
+++ Day 006/Sum.java
@@ -0,0 +1,15 @@
+package problems;
+
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        int ans = sum(20, 30);
+        System.out.println(ans3);
+    }
+
+    static int sum(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+}
