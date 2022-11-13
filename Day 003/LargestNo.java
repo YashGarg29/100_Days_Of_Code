cdaebabca9d8adb2ed33570eab73898d6694e645
YashGarg <yashgarg1106@gmail.com>
2022-11-13 13:06:26 +0530

    Update

--- Day 003/LargestNo.java
+++ Day 003/LargestNo.java
@@ -10,28 +10,6 @@ public class Largest {
         int b = in.nextInt();
         int c = in.nextInt();
         int d = in.nextInt();
-
-//        int max = a;
-//        if (b > max) {
-//            max = b;
-//        }
-//        if (c > max) {
-//            max = c;
-//        }
-//        System.out.println(max);
-
-//        int max = 0;
-//        if (a > b) {
-//            max = a;
-//        } else {
-//            max = b;
-//        }
-//        if (c > max) {
-//            max = c;
-//        }
-//        System.out.println(max);
-
-//        int max = Math.max(d, Math.max(c, Math.max(a, b)));
         int max = Math.max(c, Math.max(a, b));
         System.out.println(max);
     }
